f5f01136ade257c633a43ea0c3b32f7f0352ef9f
Jim Kalafut <jkalafut@hashicorp.com>
2020-02-26 21:54:23 -0500

    .

--- .github/actions/hello/entrypoint.sh
+++ .github/actions/hello/entrypoint.sh
@@ -7,6 +7,8 @@ echo ::set-output name=time::$time
 #git clone git@github.com:kalafut/action-test
 git checkout stable-website
 echo $time > blah
+git config --local user.email "action@github.com"
+git config --local user.name "GitHub Action"
 git add blah
 git commit -m "yay"
 git push origin stable-website
