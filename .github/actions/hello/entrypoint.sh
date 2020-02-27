13f0560045e09cee71d10cc68b18627ea3f2e7e1
Jim Kalafut <jkalafut@hashicorp.com>
2020-02-26 22:58:44 -0500

    .

--- .github/actions/hello/entrypoint.sh
+++ .github/actions/hello/entrypoint.sh
@@ -4,5 +4,7 @@ cd $GITHUB_WORKSPACE
 #git checkout stable-website
 git config --local user.email "action@github.com"
 git config --local user.name "GitHub Action"
+echo $(git rev-parse HEAD)
+echo "cherry picking $GITHUB_SHA"
 git cherry-pick $GITHUB_SHA
 git push origin stable-website
